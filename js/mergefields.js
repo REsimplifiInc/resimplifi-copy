a7998d71180fc41d327573006781715e39d7fe97
tyleryoungblood <tyler@resimplifi.com>
2017-10-26 12:59:27 -0400

    add .gitignore and test ability to save to github

--- js/mergefields.js
+++ js/mergefields.js
@@ -1,6 +1,6 @@
 var mergefields =
 [ //############################################################################
-  {'label':'header',        'value':'Buyer'},
+  {'label':'header',        'value':'Buyer!'},
   //############################################################################
 
     {'label':'subheader',                         'value':'Agency Representation'},
